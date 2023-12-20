971e929ef43a09e43b18140aad380c5c4086879d
Rene Lujano <rlujanol@nttdata.com>
2023-12-20 10:39:32 -0500

    feat(Show data) SN011 convert timestamp published data and show on screen

---
+++ News/Classes/View/ArticleCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleCell.swift
+//  News
+//
+//  Created by Rene Lujano Laura on 19/12/23.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblNewSite: UILabel!
+    @IBOutlet weak var lblPublishedDate: UILabel!
+}
+
